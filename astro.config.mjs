a9d68d0f1b997301f423a378997e9feb0f266003
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-06-25 13:35:37 -0500

    Add HTTPS link to it

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'betonjeanette.github.io',
+    site: 'https://betonjeanette.github.io',
     base: 'DivisibilityClock'
 });
