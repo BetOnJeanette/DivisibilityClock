7d054d2d02957c852a8346fe217eaadeb4bd52d7
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-06-25 23:06:31 -0500

    Add new imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://betonjeanette.github.io',
-    base: 'DivisibilityClock'
+  site: 'https://betonjeanette.github.io',
+  base: 'DivisibilityClock',
+  integrations: [solidJs()]
 });
